73376d8791c18e7ce245144bdb31e897b0dfe7a7
renatorrodrigues <renatorrodrigues2002@gmail.com>
2023-09-19 13:44:43 -0300

    Configurando tailwind e estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,27 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))'
       },
-    },
+      colors: {
+        linearGradientStart: 'hsl(249, 99%, 64%)',
+        linearGradientEnd: 'hsl(278, 94%, 30%)',
+        white: 'hsl(0, 0%, 100%)',
+        lightGrayishViolet: 'hsl(270, 3%, 87%)',
+        darkGrayishViolet: 'hsl(279, 6%, 55%)',
+        veryDarkViolet: 'hsl(278, 68%, 11%)',
+        error: 'hsl(0, 100%, 66%)'
+      },
+      fontFamily: {
+        spaceGrotesk: ['Space Grotesk', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
 export default config
