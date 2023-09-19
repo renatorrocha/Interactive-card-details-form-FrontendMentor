f91e54e77c175ad4cdad3778db031e9e33c1e1b5
renatorrodrigues <renatorrodrigues2002@gmail.com>
2023-09-19 13:59:00 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,36 @@
+import Image from 'next/image'
+import image2 from 'images/bg-main-mobile.png'
+
 export default function Home() {
   return (
-    <main className="bg-gradient-to-r from-linearGradientStart to-linearGradientEnd">
-      <h1>ola</h1>
+    <main className="">
+      <Image src={image2} alt="" />
+      <div>
+        <form action="">
+          <label htmlFor="">
+            <p>CARDHOLDER NAME</p>
+            <input type="text" />
+          </label>
+
+          <label htmlFor="">
+            <p>CARD NUMBER</p>
+            <input type="number" />
+          </label>
+
+          <div className="flex flex-row">
+            <label htmlFor="">
+              <p>EXP.DATE (MM/YY)</p>
+              <input type="text" />
+            </label>
+
+            <label htmlFor="">
+              <p>CVC</p>
+              <input type="text" />
+            </label>
+          </div>
+          <button>Confirm</button>
+        </form>
+      </div>
     </main>
   )
 }
